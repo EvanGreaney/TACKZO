f54c95842c13fc2b8fd475aa5e00caf364d60d47
EvanGreaney <G00348451@gmit.ie>
2020-04-19 21:48:09 +0100

    API working and calling down Recipes for search calories

--- src/app/Search Calories/search-calories.page.ts
+++ src/app/Search Calories/search-calories.page.ts
@@ -12,12 +12,7 @@ import { Observable } from 'rxjs';
   styleUrls: ['./search-calories.page.scss'],
 })
 export class SearchCaloriesPage implements OnInit {
-  public noOfMeals: number;
   public maxCalories: number;
-  public noOfPeople:number;
-  public calBreakfast:number;
-  public calLunch:number;
-  public calDinner:number;
   //API Stuff
   public Recipes: Array<any>;
   apiKey: String = "ccb5ae09cbc44169be9a30e8888e5e1d";
@@ -32,39 +27,49 @@ export class SearchCaloriesPage implements OnInit {
     public http: HttpClient ) 
     { 
       this.caloriesList = formBuilder.group({
-        noOfMeals: [' ', Validators.required],
         maxCalories: [' ', Validators.required],
-        noOfPeople:[' ', Validators.required],
-        calBreakfast:[' '],
-        calLunch:[' '],
-        calDinner:[' ']
       });
     }
 
   ngOnInit() {
   }
+  onFormSubmit(){
+    
+    this.maxCalories = this.caloriesList.value['maxCalories'];
+    this.storage.set("maxCalories",this.maxCalories);
+
+    console.log("max Calories in On Form submit" + this.maxCalories);
+    //sending the input to the database
+    if(!this.caloriesList.valid)
+    {
+      console.log("Form empty")
+      return false;
+    }
+    else
+    {
+        this.caloriesApi.addCalories(this.caloriesList.value)
+        .subscribe((res) => {
+          this.zone.run(() => {
+            console.log("All user input added to api")
+            this.caloriesList.reset();
+          }
+        )})
+    }//else
+
+    this.choice();
+  }
 
   choice()
   {
-    this.storage.set("noOfMeals", this.noOfMeals);
     this.storage.set("maxNoOfCal", this.maxCalories);
-    this.storage.set("noOfPeople", this.noOfPeople);
-    this.storage.set("calBreakfast", this.calBreakfast);
-    this.storage.set("calLunch", this.calLunch);
-    this.storage.set("calDinner", this.calDinner);    
     //debugging the storage of user input
-    console.log("number of meals: ", this.noOfMeals);
     console.log("Max number of calories per meal: ", this.maxCalories);
-    console.log("number of people: ", this.noOfPeople);
-    console.log("number of calories for breakfast: ", this.calBreakfast);
-    console.log("number of calories for lunch: ", this.calLunch);
-    console.log("number of calories for dinner: ", this.calDinner);
 
     this.getRecipe().subscribe(data =>{
       this.Recipes = data;
       console.log( this.Recipes);
       this.storage.set("Recipes",this.Recipes);
-      console.log("maxCalories: ",this.maxCalories);
+      console.log("maxCalories in form: ",this.maxCalories);
       console.log(this.Recipes);
       console.log("Storage: ",this.storage.get("Recipes"));
     })
@@ -85,22 +90,5 @@ export class SearchCaloriesPage implements OnInit {
     console.log("Storage: ",this.storage.get("Recipes"));
   }
   
-  onFormSubmit(){
-    //sending the input to the database
-    if(!this.caloriesList.valid)
-    {
-      console.log("Form empty")
-      return false;
-    }
-    else
-    {
-        this.caloriesApi.addCalories(this.caloriesList.value)
-        .subscribe((res) => {
-          this.zone.run(() => {
-            console.log("All user input added to api")
-            this.caloriesList.reset();
-          }
-        )})
-    }//else
-  }
+  
 }
